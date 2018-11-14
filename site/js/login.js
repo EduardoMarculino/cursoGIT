5cd80242005d854e2fae7537536b6f929e2759bd
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2018-11-14 18:49:14 -0200

    Update System Sing In

--- site/js/login.js
+++ site/js/login.js
@@ -1,13 +1,26 @@
 document.getElementById("buttonLogar").addEventListener("click", logar);
+document.getElementById("inputPassword").addEventListener("keydown", getEnter);
+
+function getEnter(event) {
+    if(event.keyCode == 13) {
+        logar();
+    }
+}
 
 function logar() {
     event.preventDefault(event);
     var email = document.getElementById("inputEmail");
     var senha = document.getElementById("inputPassword");
     var check = JSON.parse(localStorage.getItem(senha.value));
-
-    if (email.value == check.email & senha.value == check.senha) {
-        console.log("hi, "+check.email);
-        window.location.assign("./contas.html");
+    if (check) {
+        if (email.value == check.email & senha.value == check.senha) {
+            console.log("hi, "+check.email);
+            window.location.assign("./contas.html");
+        } else {
+            console.log("OI");
+            alert('Usuário ou Senha Inváildos.');
+        }
+    } else {
+        alert('Usuário Inexistente.');
     }
 }
