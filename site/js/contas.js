a6d33ea6ae9bf84b417db817343978c9116b0e09
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2018-11-22 22:39:13 -0300

    Diminuição da quantidade de variaveis inputs no contas.js

--- site/js/contas.js
+++ site/js/contas.js
@@ -3,17 +3,16 @@ $(
         $("#add").click(function() {
             event.preventDefault(event);
             /*tirar todos esses inputs por \ */
-            var input =  '<div class="col-md-6 col-sm-12 col-lg-6 col-xl-3 contas"><br>';
-                input += '<div class="card" style="width: 18rem;">';
-                input += '<img class="card-img-top" src="./img/card1.jpg" alt="Card image cap">';
-                input += '<div class="card-body">';
-                input += '<input type="text" style="border:0;font-size:20px;font-weight: 500; text-decoration:none;" value="Adicione um Título" id="valorInputTitulo"></input><br>';
-                input += '<input type="checkbox">';
-                /*input += '<span> Adicione um lembrete a essa conta</span>';*/
-                input += '<input  type="text" style="border:0;" value="Adicione um lembrete" id="valorInputLembrete"></input><br>';
-                input += '<br><br><a href="cartao.html" class="btn btn-primary">Verificar</a>';
-                input += '<a style="float:right;" class="btn btn-danger deletar" href="#" role="button">Deletar</a>';
-                input += '</div></div></div>';
+            var input =  '<div class="col-md-6 col-sm-12 col-lg-6 col-xl-3 contas"><br>\
+                            <div class="card" style="width: 18rem;">\
+                                <img class="card-img-top" src="./img/card1.jpg" alt="Card image cap">\
+                                <div class="card-body">\
+                                    <input type="text" style="border:0;font-size:20px;font-weight: 500; text-decoration:none;" value="Adicione um Título" id="valorInputTitulo"></input><br>\
+                                    <input type="checkbox">\
+                                    <input  type="text" style="border:0;" value="Adicione um lembrete" id="valorInputLembrete"></input><br>\
+                                    <br><br><a href="cartao.html" class="btn btn-primary">Verificar</a>\
+                                    <a style="float:right;" class="btn btn-danger deletar" href="#" role="button">Deletar</a>\
+                         </div></div></div>';
 
                 $("#campos").append(input);
                 return false;
