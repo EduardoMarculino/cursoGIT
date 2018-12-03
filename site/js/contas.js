dadfa29b1aa222b455fbabd66cfe2fa79eeed388
Matheus Maximiano de Melo Vieira <matheusmaximianomv@aluno.fapce.edu.br>
2018-12-03 17:30:56 -0300

    BUG Firefox

--- site/js/contas.js
+++ site/js/contas.js
@@ -1,30 +1,28 @@
-$(
-    function() {
-        $("#add").click(function() {
-            event.preventDefault(event);
-            var input =  '<div class="col-md-6 col-sm-12 col-lg-6 col-xl-3 contas animated fadeInLeft" id="valeuFalou"><br>\
-                            <div class="card" style="width: 18rem;">\
-                                <img class="card-img-top" src="./img/img.png" alt="Card image cap">\
-                                <hr>\
-                                <div class="card-body">\
-                                    <input type="text" style="border:0;font-size:20px;font-weight: 500; text-decoration:none;" value="Adicione um Título" id="valorInputTitulo"></input><br>\
-                                    <input type="checkbox">\
-                                    <input  type="text" style="border:0;" value="Adicione um lembrete" id="valorInputLembrete"></input><br>\
-                                    <br><br><a href="cartao.html" class="btn btn-primary">Verificar</a>\
-                                    <a style="float:right;" class="btn btn-danger deletar" href="#" role="button">Deletar</a>\
-                         </div></div></div>';
 
-            $("#campos").append(input);
-            return false;
-        });
+$("#add").click(function(event) {
+    event.preventDefault();
+    
+    var input =  '<div class="col-md-6 col-sm-12 col-lg-6 col-xl-3 contas animated fadeInLeft" id="valeuFalou"><br>\
+                    <div class="card" style="width: 18rem;">\
+                        <img class="card-img-top" src="./img/img.png" alt="Card image cap">\
+                        <hr>\
+                        <div class="card-body">\
+                            <input type="text" style="border:0;font-size:20px;font-weight: 500; text-decoration:none;" value="Adicione um Título" id="valorInputTitulo"></input><br>\
+                            <input type="checkbox">\
+                            <input  type="text" style="border:0;" value="Adicione um lembrete" id="valorInputLembrete"></input><br>\
+                            <br><br><a href="cartao.html" class="btn btn-primary">Verificar</a>\
+                            <a style="float:right;" class="btn btn-danger deletar" href="#" role="button">Deletar</a>\
+                    </div></div></div>';
 
-        $(document).on('click', '.deletar',function() {
-            event.preventDefault(event);
-            var _this_ = $(this);
-            $(this).parent().parent().parent().addClass('animated zoomOut');
-            setTimeout(function(){
-                _this_.parent().parent().parent().remove();
-            },500);
-        });
-    }
-);
+    $("#campos").append(input);
+    return false;
+});
+
+$(document).on('click', '.deletar',function() {
+    event.preventDefault(event);
+    var _this_ = $(this);
+    $(this).parent().parent().parent().addClass('animated zoomOut');
+    setTimeout(function(){
+        _this_.parent().parent().parent().remove();
+    },500);
+});
